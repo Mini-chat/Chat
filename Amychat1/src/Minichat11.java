4538bb74062e24a1647939d9b8bdfe8d1503c991
Fefali <AbelmalickFefali1011@gmail.com>
2018-01-12 17:31:04 +0000

    ufjy

--- Amychat1/src/Minichat11.java
+++ Amychat1/src/Minichat11.java
@@ -4,7 +4,7 @@ public class Minichat11 {
 		
 		
 			System.out.println("Hello alya lol");
-			System.out.println("Hello amy hhlhhhhhojjl");
+			System.out.println("Hello amy hhojjl");
 			
 			System.out.println("Hello groupe");
 			System.out.println("Yemii 3avak Amy ;) & Ma7voudha and aaliya");
