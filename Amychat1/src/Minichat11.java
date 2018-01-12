b2faacc20a4c2228167915a560e6c5acac226a31
Fefali <AbelmalickFefali1011@gmail.com>
2018-01-12 18:54:30 +0000

    uiu

--- Amychat1/src/Minichat11.java
+++ Amychat1/src/Minichat11.java
@@ -7,7 +7,11 @@ public class Minichat11 {
 			System.out.println("Hello amy hhojjl");
 			
 			System.out.println("Hello groupe");
+<<<<<<< HEAD
 			System.out.println("Yemii 3avak Amy ;) & Ma7voudha and aaliya");
+=======
+			System.out.println("tfou amyy");
+>>>>>>> refs/remotes/origin/master
 	}
 
 }
